bb4d169f5c79c0ef04dc34cae4969c0ba3909ce5
chunlee-thong <chunlee.thong@gmail.com>
2024-04-04 22:15:31 +0700

    update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import rss from "@astrojs/rss";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
+import { SITE_TITLE, getMyDescription } from "../config";
 
 export async function get(context) {
   const blog = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    description: getMyDescription(),
     site: import.meta.env.SITE,
     items: blog.map((post) => ({
       title: post.data.title,
